ccc55b029d8d04b4ba7296f528f367b65bd03a57
uvellani <unni.vellani@gmail.com>
2015-03-21 11:32:15 -0400

    Code changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+#######################################################
+#These two functions demonstrate loading and retrieving
+#cached values
+#
+#Example run commands
+#> a<-makeCacheMatrix()
+#> a$setMatrix(matrix(1:4,2,2))
+#> cacheSolve(a)
+#
+#######################################################
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function creates the cache and the setters and getters
+makeCacheMatrix <- function(m = matrix()) {
+        #initialize
+        m<-NULL
+        x<-NULL
+        
+        #setter - matrix cache
+        setMx<-function(m){
+                m<<-m
+        }
+        
+        #getter - matrix
+        getMx<-function() m
+        
+        #setter - inverse matrix cache
+        setIMx<-function(x) {
+                x<<-x
+        }
+        
+        #getter - inverse matrix
+        getIMx<-function() x
+        
+        #return the getters and setters as a list
+        list(setMatrix=setMx, 
+             getMatrix=getMx, 
+             setInvMatrix=setIMx,
+             getInvMatrix=getIMx)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#This function sets and gets the cached values of the inverted matrix
+cacheSolve <- function(x=matrix()) {
+        #get inverse matrix
+        imx<-x$getInvMatrix()
+        
+        #if its not in the cache load it
+        if(is.null(imx)){
+                message("Loaded into cache.")
+                #load the inverse cache
+                imx<-x$setInvMatrix(solve(x$getMatrix()))
+        } 
+        
+        #return cached value
+        imx
+        
 }
